df3057719c73a733110177bac8ff1bd5a8c8bc87
siddharth8128 <siddharthagrawal8128@gmail.com>
2025-04-07 13:41:53 +0530

    update

--- src/Pages/editdetail.jsx
+++ src/Pages/editdetail.jsx
@@ -35,22 +35,8 @@ function Editdetail({ selectedActivity, onUpdate, onclose }) {
       [event.target.name]: event.target.value,
     });
   };
-  const activitylistref = collection(db, "activities");
 
-  const onsubmit = async () => {
-    try {
-      await addDoc(activitylistref, {
-        activityname: data.name,
-        contributors: data.cont,
-        description: data.desc,
-        funds: data.funds,
-        location: data.loc,
-        date: data.date,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  
 
   const handleSubmit = async () => {
     try {
@@ -94,76 +80,52 @@ function Editdetail({ selectedActivity, onUpdate, onclose }) {
 
         <Grid container>
           <Grid item xs={12} sm={12} md={4} key={0} sx={gridStyles}>
-            <div className="box1">
-              <TextField
-                id="filled-text"
-                label="Activity Name"
-                type="text"
-                variant="outlined"
-                name="name"
-                onChange={handleChange}
-                fullWidth
-              />
-            </div>
-          </Grid>
-          <Grid item xs={12} sm={12} md={4} lg={4} key={1} sx={gridStyles}>
-            <LocalizationProvider dateAdapter={AdapterDayjs} className="date">
-              <DatePicker
-                onChange={handleChange}
-                label="Start Date"
-                style={{ width: "100%" }}
-              />
-            </LocalizationProvider>
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={4} key={3} sx={gridStyles}>
-            <TextField
-              id="outlined-multiline-static"
-              label="Location"
-              multiline
-              placeholder="address"
-              name="loc"
-              onChange={handleChange}
-              fullWidth
-            />
-            {/* </div> */}
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={4} key={5} sx={gridStyles}>
-            {/* <div className="box2"> */}
-            <TextField
-              id="filled-text"
-              label="Funds Required"
-              type="Number"
-              name="funds"
-              onChange={handleChange}
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={4} key={6} sx={gridStyles}>
-            <TextField
-              id="filled-text"
-              label="Contributors Required"
-              type="Number"
-              name="cont"
-              onChange={handleChange}
-              fullWidth
-            />
-            {/* </div> */}
-          </Grid>
-
-          <Grid item xs={12} sm={12} md={4} key={4} sx={gridStyles}>
-            <TextField
-              id="outlined-multiline-static"
-              label="Description"
-              multiline
-              rows={4}
-              placeholder="Description"
-              name="desc"
-              onChange={handleChange}
-              fullWidth
-            />
+            <div className="box1"
+            TextField
+            label='Activity Name'
+            name='activityname'
+            value={formData.activityname}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label='Contributors'
+            name='contributors'
+            type='number'
+            value={formData.contributors}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label='Description'
+            name='description'
+            value={formData.description}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label='Funds'
+            name='funds'
+            type='number'
+            value={formData.funds}
+            onChange={handleChange}
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label='Location'
+            name='location'
+            value={formData.location}
+            onChange={handleChange}
+            fullWidth
+          />
           </Grid>
         </Grid>
 
